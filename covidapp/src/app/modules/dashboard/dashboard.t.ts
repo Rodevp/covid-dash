f65b5bdbf60f39c7f71486383b621a04f83bd56b
Rodevp <rodriguezrodrigor12@gmail.com>
2023-06-23 18:04:06 -0500

    parse data for dashboard :white_check_mark:

---
+++ covidapp/src/app/modules/dashboard/dashboard.t.ts
@@ -0,0 +1,20 @@
+export interface Sheet {
+    Admin2: String
+    Combined_Key: String
+    Country_Region: String
+    Province_State: String
+    iso2: String
+    iso3: String
+    FIPS: number
+    Lat: number
+    Long_: number
+    Population: number
+    UID: number
+    code3: number
+}
+
+export interface DateObject {
+    date: string,
+    valueDate: number
+}
+
--- covidapp/src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ covidapp/src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from "xlsx";
+import { DateObject, Sheet } from '../dashboard.t';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,29 +11,91 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
+  dataSheets: Sheet[] = [];
+
   redCsv(event: any) {
+
     const file = event?.target?.files[0];
 
     const reader = new FileReader();
     reader.readAsBinaryString(file);
-    
+
 
     reader.addEventListener('load', (e) => {
 
-      const workbook = XLSX.read(reader.result, {type: "binary"});
+      const workbook = XLSX.read(reader.result, { type: "binary" });
 
       const sheetsNames: String[] = workbook.SheetNames;
 
       const sheet = sheetsNames[0];
 
-      const data = XLSX.utils.sheet_to_json(workbook.Sheets[`${sheet}`]);
+      this.dataSheets = XLSX.utils.sheet_to_json(workbook.Sheets[`${sheet}`]);
 
-      console.log('data csv -> ', data);
+      this.parseObjectForDateAndPopulation(this.dataSheets);
+
+    })
+
+  }
+
+  parseObjectForDateAndPopulation(data: Sheet[]) {
+
+    const dataOfPopulation = data.map(sheet => this.evaluateObjectKeys(sheet))
+
+  }
+
+  evaluateObjectKeys(object: Sheet) {
+
+    const deathsByDate = this.getKeysForDates(object);
+    const provinceAndPopulation = this.getPopulationAndState(object);
+
+    const data = {
+      deathsByDate,
+      ...provinceAndPopulation
+    }
+
+    console.log(data)
+
+  }
+
+
+  getKeysForDates(object: Sheet) {
+
+    const dates = Object.keys(object).slice(12, -1);
+    const dateValues = Object.values(object).slice(12, -1);
+
+    const arrayObjectsOfDate: DateObject[] = [];
+
+    dates.forEach((date, i) => {
+
+      const dateObject = {
+        date: date,
+        valueDate: dateValues[i]
+      }
+
+      arrayObjectsOfDate.push(dateObject);
 
     })
 
 
+    return arrayObjectsOfDate
+
   }
+
+
+  getPopulationAndState(object: Sheet) {
+    
+    const valuePopulation = Object.values(object).slice(11, 12);
+    const provinceStateValue = Object.values(object).slice(6, 7);
+    
+    return {
+      provinceState: provinceStateValue[0],
+      population: valuePopulation[0]
+    }
+ 
+  }
+
+
+
 
   ngOnInit(): void {
   }
