ac0c3fa34b72f4f480d330364f7baf34f29f1365
Rodevp <rodriguezrodrigor12@gmail.com>
2023-06-23 10:23:29 -0500

    dashboard and login module ready :fire:

--- covidapp/src/app/app.module.ts
+++ covidapp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NgChartsModule } from "ng2-charts"; 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +15,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     NgChartsModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
