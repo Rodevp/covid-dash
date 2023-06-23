371e60205124d065b239e767506038c0f36a6ccc
Rodevp <rodriguezrodrigor12@gmail.com>
2023-06-23 15:57:35 -0500

    read csv file :fire:

--- covidapp/src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ covidapp/src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from "xlsx";
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() { }
+
+  redCsv(event: any) {
+    const file = event?.target?.files[0];
+
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    
+
+    reader.addEventListener('load', (e) => {
+
+      const workbook = XLSX.read(reader.result, {type: "binary"});
+
+      const sheetsNames: String[] = workbook.SheetNames;
+
+      const sheet = sheetsNames[0];
+
+      const data = XLSX.utils.sheet_to_json(workbook.Sheets[`${sheet}`]);
+
+      console.log('data csv -> ', data);
+
+    })
+
+
+  }
 
   ngOnInit(): void {
   }
