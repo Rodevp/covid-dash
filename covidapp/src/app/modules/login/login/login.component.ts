99969ffa9f374b28202726a6a4c9bce05b91d97b
Rodevp <rodriguezrodrigor12@gmail.com>
2023-06-24 01:08:15 -0500

    routing push to dashboard :tada:

--- covidapp/src/app/modules/login/login/login.component.ts
+++ covidapp/src/app/modules/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: Router
+  ) { }
+
+  navigateToDashboard(event: any) {
+    event.preventDefault();
+    this.route.navigate(['/dashboard']);
+  }
 
   ngOnInit(): void {
   }
